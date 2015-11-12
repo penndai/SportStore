2c932c934ad0ec073515f161358088e52c2af64e
Penn Dai <penndai@hotmail.com>
2015-11-12 22:36:18 +1100

    add new project to git hub for asp.net mvc web api 2

---
+++ SportStore/App_Start/IdentityConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SportStore.IdentityConfig))]
+
+namespace SportStore
+{
+  
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app) { }
+    }
+}
---
+++ SportStore/Controllers/PreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportStore.Controllers
+{
+    public class PreController : Controller
+    {
+        // GET: Pre
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
