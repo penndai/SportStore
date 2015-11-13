cb29a9370d290f3b57c6e18348d2794b42d34678
Penn Dai <penndai@hotmail.com>
2015-11-13 22:37:38 +1100

    Add data model classes

--- SportStore/Controllers/PreController.cs
+++ SportStore/Controllers/PreController.cs
@@ -1,17 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
+using SportStore.Models;
 
 namespace SportStore.Controllers
 {
     public class PreController : Controller
     {
+        IRepository repo;
+
+        public PreController()
+        {
+            repo = new ProductRepository();
+        }
+
         // GET: Pre
         public ActionResult Index()
         {
-            return View();
+            return View(repo.Products);
+        }
+
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            await repo.DeleteProductAsync(id);
+            return RedirectToAction("Index");
+        }
+        public async Task<ActionResult> SaveProduct(Product product)
+        {
+            await repo.SaveProductAsync(product);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Orders()
+        {
+            return View(repo.Orders);
+        }
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            await repo.DeleteOrderAsync(id);
+            return RedirectToAction("Orders");
+        }
+        public async Task<ActionResult> SaveOrder(Order order)
+        {
+            await repo.SaveOrderAsync(order);
+            return RedirectToAction("Orders");
         }
     }
 }
